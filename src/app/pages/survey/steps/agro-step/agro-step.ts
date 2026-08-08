5a0a6e8c56df2aee77b56b01f2ed38975a8891e2
Amilcar <pintadoamilcar2001@gmail.com>
2026-08-07 22:21:40 -0500

    Cambios en configuración y componente map

--- src/app/pages/survey/steps/agro-step/agro-step.ts
+++ src/app/pages/survey/steps/agro-step/agro-step.ts
@@ -5,6 +5,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { PolygonDrawerComponent } from '../../../../shared/components/polygon-drawer/polygon-drawer';
 @Component({
   selector: 'app-agro-step',
   standalone: true,
@@ -14,7 +16,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule
   ],
   templateUrl: './agro-step.html',
   styleUrls: ['./agro-step.scss'],
@@ -25,6 +28,7 @@ export class AgroStepComponent implements OnInit, OnChanges {
 
   form!: FormGroup;
   private fb = inject(FormBuilder);
+  private dialog = inject(MatDialog);
 
   get cultivos(): FormArray {
     return this.form.get('cultivos') as FormArray;
@@ -62,7 +66,8 @@ export class AgroStepComponent implements OnInit, OnChanges {
     const cultivoGroup = this.fb.group({
       nombre_cultivo: ['', Validators.required],
       area_ha: [0, [Validators.required, Validators.min(0.01)]],
-      rendimiento_estimado: [null] // opcional
+      rendimiento_estimado: [null],// opcional
+      geom: [null] 
     });
     this.cultivos.push(cultivoGroup);
   }
@@ -71,5 +76,28 @@ export class AgroStepComponent implements OnInit, OnChanges {
     if (this.cultivos.length > 1) {
       this.cultivos.removeAt(index);
     }
+  }
+   openPolygonDrawer(index: number): void {
+    const dialogRef = this.dialog.open<PolygonDrawerComponent>(PolygonDrawerComponent, {
+      width: '90%',
+      maxWidth: '800px',
+      height: '90%',
+      maxHeight: '600px'
+    });
+
+    dialogRef.componentInstance.polygonSaved.subscribe((geojson: any) => {
+      if (geojson) {
+        const cultivoForm = this.cultivos.at(index);
+        cultivoForm.patchValue({ geom: geojson });
+        console.log('✅ Polígono guardado para cultivo', index, geojson);
+      } else {
+        console.log('ℹ️ No se dibujó polígono');
+      }
+      dialogRef.close();
+    });
+
+    dialogRef.componentInstance.cancelled.subscribe(() => {
+      dialogRef.close();
+    });
   }
 }
